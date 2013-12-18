47d714c4b0d59d72e20cce5b944323a3b89bc7b8
Francesco Mattia <francesco.mattia@akqa.com>
2013-12-18 16:57:35 +0000

    fix again

--- myiosproject/ViewController.m
+++ myiosproject/ViewController.m
@@ -18,7 +18,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"push a change!
+    NSLog(@"push a change!");
 }
 
 - (void)didReceiveMemoryWarning
