4a62177d4fd35bbdb4480527716fdc6e56d54b72
Francesco Mattia <francesco.mattia@akqa.com>
2013-12-19 11:04:00 +0000

    Gemfile, Podfile, kiwi integration, first unit tests

---
+++ Gemfile
@@ -0,0 +1,2 @@
+source 'https://rubygems.org'
+gem "ocunit2junit", "1.2"
---
+++ myiosprojectTests/SampleSpec.m
@@ -0,0 +1,21 @@
+//
+//  SampleSpec.m
+//  myiosproject
+//
+//  Created by Francesco Mattia on 19/12/2013.
+//  Copyright (c) 2013 Francesco Mattia. All rights reserved.
+//
+
+#import "Kiwi.h"
+
+SPEC_BEGIN(TestSpec)
+
+describe(@"Math", ^{
+    it(@"is pretty cool", ^{
+        NSUInteger a = 16;
+        NSUInteger b = 26;
+        [[theValue(a + b) should] equal:theValue(42)];
+    });
+});
+
+SPEC_END
